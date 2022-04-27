46f60d8c30b53a66ba087b0e4959015f82b2ddd1
raaipag <pagnanraiane@gmail.com>
2022-04-26 21:48:23 -0300

    reserva de restaurante

--- server.js
+++ server.js
@@ -1,79 +1,120 @@
 //1° Importar os módulos necessários---------------------------------------------------------
-//nomeia as const
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const { Console } = require("console");
 
 //2° Contruir o objeto express (ele vai colocar nossa aplicação para rodar)------------------
 const app = express();
 
 app.use(bodyParser.json());
 
-//const jsonParser = bodyParser.json();
-
 //3° Configurar a porta que o servidor atende e colocar em execução--------------------------
 const porta = 3000;
-app.set('port', porta);
-
+app.set("port", porta);
 
 //4°Subindo o servidor-----------------------------------------------------------------------
 const server = http.createServer(app);
 server.listen(3000);
 console.log("O servidor esta rodando na porta " + porta);
 
-//5° Definindo as variaveis da lista---------------------------------------------------------
-let id = 2;
-let alunos = [
-    {
-        id: 1,
-        nome: "Matheus",
-        fone: "123456789",
-        email: "matheus@email.com"
-    },
-    {
-        id: 2,
-        nome: "Ana",
-        fone: "987654321",
-        email: "ana@gmail.com"
-    }
+const clientes = [
+  {
+    id_cliente: 0,
+    nome: "João da silva",
+    data_nasci: "22/05/1964",
+    celular: "(11) 99888-0987",
+    email: "joao@mailinator.com",
+  },
+  {
+    id: 1,
+    nome: "Maria Joana",
+    data_nasci: "30/07/1999",
+    celular: "(11) 99488-0987",
+    email: "maria@mailinator.com",
+  },
+];
+
+const agenda = [
+  {
+    id_agenda: 0,
+    data: "30-05-2022",
+    hora: "08:00:00",
+    disponibilidade: 10,
+  },
+  {
+    id_agenda: 1,
+    data: "30-05-2022",
+    hora: "08:40:00",
+    disponibilidade: 10,
+  },
+  {
+    id_agenda: 2,
+    data: "30-05-2022",
+    hora: "09:00:00",
+    disponibilidade: 10,
+  },
+  {
+    id_agenda: 3,
+    data: "30-05-2022",
+    hora: "09:40:00",
+    disponibilidade: 10,
+  },
+  {
+    id_agenda: 4,
+    data: "30-05-2022",
+    hora: "10:00:00",
+    disponibilidade: 10,
+  },
+  {
+    id_agenda: 5,
+    data: "30-05-2022",
+    hora: "10:40:00",
+    disponibilidade: 10,
+  },
 ];
 
 //6° Tratar as requisições HTTP do tipo POST---------------------------------------------------
-app.post("/alunos", (req, res, next) =>{
-    const aluno = {
-        id: id+=1,
-        nome: req.body.nome,
-        fone: req.body.fone,
-        email: req.body.email
-    };
-    alunos.push(aluno);
-    res.status(200).json(aluno);
-})
-
-//7° Tratar as requisições HTTP do tipo GET----------------------------------------------------
-app.get("/alunos", (req, res, next)=>{
-    res.status(200).json(alunos);
-})
-
-//8° Tratar as requisições HTTP do tipo PUT----------------------------------------------------
-app.put("/alunos", (req, res, next) =>{
-    alunos.forEach((aluno) => {
-        if(aluno.id === req.body.id){
-            aluno.fone = req.body.fone
-        }
-    })
-    res.status(204).end();
+app.post("/cliente", (req, res, next) => {
+  const cliente = {
+    id: (id += 1),
+    nome: req.body.nome,
+    data_nasci: req.body.data_nasci,
+    celular: req.body.celular,
+    email: req.body.email,
+  };
+  clientes.push(cliente);
+  res.status(200).json(clientes);
 });
 
-//9° Tratar as requisições HTTP do tipo DELETE------------------------------------------------ 
-app.delete("/alunos", (req, res, next) =>{
-    let indice = alunos.findIndex((obj) => obj.id == req.body.id);
-    if(indice >= 0){
-        alunos.splice(indice, 1);
-        res.status(200).json(alunos);
-    }else{
-        res.status(204).end();
+//7° Tratar as requisições HTTP do tipo GET----------------------------------------------------
+app.get("/agenda", (req, res, next) => {
+  res.status(200).json(agenda);
+});
+
+app.get("/agenda/:id", (req, res, next) => {
+  const idAgenda = req.params.id;
+
+  console.log("ID AGENDA", idAgenda);
+
+  agenda.forEach((item) => {
+    const disponibilidade = item.disponibilidade;
+
+    if (item.id_agenda == idAgenda && disponibilidade > 0) {
+      item.disponibilidade = disponibilidade - 1
+      console.log("Como ficou:::::::", item);
+      res.status(200).json(agenda);
     }
+  });
+});
+
+app.post("/reserva", (req, res, next) => {
+  const reserva = {
+    id_reserva: req.body.id_agenda + 1,
+    nome_cliente: req.body.nome,
+    id_agenda: req.body.id_agenda,
+    disponibilidade: req.body.disponibilidade,
+  };
 });
